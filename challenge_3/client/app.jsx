84f1b0311a3cacbb75e2ffcb4e48b4712d7678ec
Laurence <ldn221@nyu.edu>
2019-11-20 17:11:33 -0800

    Progress on Multi-Step Checkout

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,182 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      page: 0,
+      account: {
+        name: '',
+        email: '',
+        password: '',
+        phoneNumber: ''
+      },
+      address: {
+        line1: '',
+        line2: '',
+        city: '',
+        state: '',
+        zip: ''
+      },
+      creditCard: {
+        cardNumber: '',
+        expiry: '',
+        cvv: '',
+        billingZip: ''
+      }
+    };
 
+    this.nextPage = this.nextPage.bind(this);
+    this.purchase = this.purchase.bind(this);
+  }
+
+  nextPage() {
+    var currentPage = this.state.page;
+    if (document.getElementById('myForm') !== null) {
+      var cool = document.getElementById('myForm').elements;
+
+      var newInfo = {}
+      Array.from(cool).forEach((input) => {
+        newInfo[input.name] = input.value;
+      });
+
+      var newState = {
+        page: currentPage + 1
+      };
+      if (currentPage === 1) {
+        newState.account = newInfo;
+      }
+      if (currentPage === 2) {
+        newState.address = newInfo;
+      }
+      if (currentPage === 3) {
+        newState.creditCard = newInfo;
+      }
+
+      this.setState(newState);
+
+    } else {
+      this.setState({page: currentPage + 1});
+    }
+  }
+
+  purchase() {
+    this.setState({page: 0});
+  }
+
+  render() {
+      if (this.state.page === 0) return <Home next={this.nextPage} />;
+      if (this.state.page === 1) return <F1 next={this.nextPage} />;
+      if (this.state.page === 2) return <F2 next={this.nextPage} />;
+      if (this.state.page === 3) return <F3 next={this.nextPage} />;
+      if (this.state.page === 4) return <Summary purchase={this.purchase} info={this.state}/>;
+  }
+}
+
+let Home = (props) => {
+  return (
+    <div id="home">
+      <p>Home</p>
+      <button onClick={props.next} >Go to Checkout!</button>
+    </div>
+  );
+};
+
+let F1 = (props) => {
+  return (
+    <div>
+      <form id="myForm">
+        <table>
+            <thead><tr><th scope="col" colSpan="2">Account Info</th></tr></thead>
+            <tbody>
+            <tr><th scope="row">Name</th><td><input name="name" type="text" placeholder="First and Last"></input></td></tr>
+            <tr><th scope="row">Email</th><td><input name="email" type="text" placeholder="example: abc@123.com"></input></td></tr>
+            <tr><th scope="row">Password</th><td><input name="password" type="text" placeholder="password..."></input></td></tr>
+            <tr><th scope="row">Phone #</th><td><input name="phoneNumber" ype="text" placeholder="###-###-####"></input></td></tr>
+            </tbody>
+        </table>
+      </form>
+      <p></p>
+      <button onClick={props.next} >Next</button>
+    </div>
+  );
+};
+
+let F2 = (props) => {
+  return (
+    <div>
+      <form id="myForm">
+        <table>
+            <thead><tr><th scope="col" colSpan="2">Shipping Address</th></tr></thead>
+            <tbody>
+            <tr><th scope="row">Line 1</th><td><input name="line1" type="text" placeholder="line 1"></input></td></tr>
+            <tr><th scope="row">Line 2</th><td><input name="line2" type="text" placeholder="line 2"></input></td></tr>
+            <tr><th scope="row">City</th><td><input name="city" type="text" placeholder="City"></input></td></tr>
+            <tr><th scope="row">State</th><td><input name="state" type="text" placeholder="State"></input></td></tr>
+            <tr><th scope="row">Zip</th><td><input name="zip" type="text" placeholder="Zip"></input></td></tr>
+            </tbody>
+        </table>
+      </form>
+      <p></p>
+      <button onClick={props.next} >Next</button>
+    </div>
+  );
+};
+
+let F3 = (props) => {
+  return ( 
+    <div>
+      <form id="myForm">
+        <table>
+            <thead><tr><th scope="col" colSpan="2">Credit Card</th></tr></thead>
+            <tbody>
+            <tr><th scope="row">Card #</th><td><input name="cardNumber" type="text" placeholder="###############"></input></td></tr>
+            <tr><th scope="row">Expiry</th><td><input name="expiry" type="text" placeholder="MM/YY"></input></td></tr>
+            <tr><th scope="row">CVV</th><td><input name="cvv" type="text" placeholder="cvv"></input></td></tr>
+            <tr><th scope="row">Billing Zip</th><td><input name="billingZip" type="text" placeholder="Billing Zip"></input></td></tr>
+            </tbody>
+        </table>
+      </form>
+      <p></p>
+      <button onClick={props.next} >To Summary</button>
+    </div>
+  );
+};
+
+let Summary = (props) => {
+  return ( 
+    <div>
+      <table>
+        <thead><tr><th scope="col" colSpan="2">Account Info</th></tr></thead>
+        <tbody>
+          <tr><th scope="row">Name</th><td>{props.info.account.name}</td></tr>
+          <tr><th scope="row">Email</th><td>{props.info.account.email}</td></tr>
+          <tr><th scope="row">Password</th><td>{props.info.account.password}</td></tr>
+          <tr><th scope="row">Phone #</th><td>{props.info.account.phoneNumber}</td></tr>
+        </tbody>
+      </table>
+      <table>
+        <thead><tr><th scope="col" colSpan="2">Shipping Address</th></tr></thead>
+        <tbody>
+          <tr><th scope="row">Line 1</th><td>{props.info.address.line1}</td></tr>
+          <tr><th scope="row">Line 2</th><td>{props.info.address.line2}</td></tr>
+          <tr><th scope="row">City</th><td>{props.info.address.city}</td></tr>
+          <tr><th scope="row">State</th><td>{props.info.address.state}</td></tr>
+          <tr><th scope="row">Zip</th><td>{props.info.address.zip}</td></tr>
+        </tbody>
+      </table>
+      <table>
+        <thead><tr><th scope="col" colSpan="2">Credit Card</th></tr></thead>
+        <tbody>
+          <tr><th scope="row">Card #</th><td>{props.info.creditCard.cardNumber}</td></tr>
+          <tr><th scope="row">Expiry</th><td>{props.info.creditCard.expiry}</td></tr>
+          <tr><th scope="row">CVV</th><td>{props.info.creditCard.cvv}</td></tr>
+          <tr><th scope="row">Billing Zip</th><td>{props.info.creditCard.billingZip}</td></tr>
+        </tbody>
+      </table>
+      <p></p>
+      <button onClick={props.purchase} >Confirm Purchase</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,32 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+
+const server = express();
+
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.post('/checkout', (req, res) => {
+
+});
+
+server.post('/info', (req, res) => {
+
+});
+
+server.get('/info', (req, res) => {
+
+});
 
 
+const port = 8000;
+server.listen(port, () => {
+    console.log('Checkout server is running!');
+    console.log(`Listening on port ${port}...`);
+    console.log(`http://localhost:${port}`);
+
+    var mongoDB = 'mongodb://127.0.0.1/accountInfo';
+    mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+});
